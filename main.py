5b995a019e4fda4b7577b8d3f2f62f43ea17a473
Maxs Shel <Maxs200099@gmail.com>
2020-01-27 16:53:01 +0300

    adding some new fitches

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import json
-import os
 import ftplib
+import threading
 from threading import Thread
 
 
 class MyThread(Thread):
-    def __init__ (self, file_name, root, number_of_thread):
+    def __init__ (self, file_name, path_from, path_to, number_of_thread):
         """Init of Thread"""
         Thread.__init__(self)
         self.number_of_thread = number_of_thread
         self.file_name = file_name
+        self.path_from = path_from
+        self.path_to = path_to
         # login to ftp server
         self.ftp = ftplib.FTP(
             config[ 'settings_for_ftp' ][ 'host' ],
@@ -20,31 +22,36 @@ class MyThread(Thread):
         self.ftp.encoding = 'utf-8'
         # change home directory to directory 'path_to_copy_on_server'
         try:
-            self.ftp.cwd(config[ 'settings_for_files' ][ 'path_to_copy_on_server' ])
+            self.ftp.cwd(self.path_to)
         # if directory not found, creating new directory and change directory
         except ftplib.error_perm:
-            self.ftp.mkd(config[ 'settings_for_files' ][ 'path_to_copy_on_server' ])
-            self.ftp.cwd(config[ 'settings_for_files' ][ 'path_to_copy_on_server' ])
-        self. full_file_name = os.path.join(root, file_name)
+            self.ftp.mkd(self.path_to)
+            self.ftp.cwd(self.path_to)
 
     def run(self):
         """Starting thread"""
-        ftpresponse = self.ftp.storbinary("STOR %s" % self.file_name, open(self.full_file_name, 'rb'))
-        print(self.number_of_thread, self.file_name, ftpresponse[ 4: ])
-        self.ftp.quit()
+        threadLimiter.acquire()
+        try:
+            ftpresponse = self.ftp.storbinary("STOR %s" % self.file_name, open(self.path_from, 'rb'))
+            print(self.number_of_thread, self.file_name, ftpresponse[ 4: ])
+            self.ftp.quit()
+        finally:
+            threadLimiter.release()
 
 
 def create_threads():
     """Creating threads"""
-    for root, dirs, files in os.walk(config[ 'settings_for_files' ][ 'path_to_files' ]):
-        number_of_thread = 0
-        for file_name in files:
-            number_of_thread += 1
-            my_thread = MyThread(file_name, root, number_of_thread)
-            my_thread.start()
+    number_of_thread = 0
+    for file_name in config[ 'settings_for_files' ]:
+        number_of_thread += 1
+        path_from = config[ 'settings_for_files' ][file_name]['path_from']
+        path_to = config[ 'settings_for_files' ][file_name]['path_to']
+        my_thread = MyThread(file_name, path_from, path_to,  number_of_thread)
+        my_thread.start()
 
 
 if __name__ == "__main__":
     with open('config.json') as json_file:
         config = json.load(json_file)
+    threadLimiter = threading.BoundedSemaphore(int(config[ 'settings_for_threads' ][ 'number_of_threads' ]))
     create_threads()
